a6a56f5ebe0c7322fdf10a94bd8ed33f85a742d4
JorgeGT <jorgarti@gmail.com>
2014-06-16 14:41:40 +0200

    Initial commit
    =)

---
+++ JCDstatusPlot.m
@@ -0,0 +1,114 @@
+%% JCDstatusPlot
+%  How to read and use data from Xively IoT API
+%  Copyright (C) 2014  Jorge Garcia Tiscar
+% 
+%  This program is free software: you can redistribute it and/or modify
+%  it under the terms of the GNU General Public License as published by
+%  the Free Software Foundation; either version 3 of the License, or
+%  (at your option) any later version (see LICENSE).
+
+%% Initialize
+clc
+close all
+clear all
+addpath Auxiliary % Put all required files here
+
+%% Get data
+load       ./Auxiliary/apiKey
+contract = 'Valence';
+url      = 'https://api.jcdecaux.com/vls/v1/stations';
+rawData  = urlread([url '?contract=' contract '&apiKey=' apiKey ]);
+stations = cell2mat(loadjson(rawData));
+
+%%  Compute stats
+fullSt       = sum([stations(:).available_bike_stands] == 0);
+emptySt      = sum([stations(:).available_bikes] == 0);
+availBikes   = sum([stations(:).available_bikes]);
+availStands  = sum([stations(:).available_bike_stands]);
+totalStands  = sum([stations(:).bike_stands]);
+
+%  Percentages
+fullStP      = round(fullSt  / length(stations) *100);
+emptyStP     = round(emptySt / length(stations) *100);
+availBikesP  = round(availBikes  / totalStands *100);
+availStandsP = round(availStands / totalStands *100);
+
+%  Most populated station
+[~,sortSt]   = sort([stations(:).available_bikes],'descend');
+mostPopSt    = stations(sortSt(1)).address;
+
+%%  Compute last update on local time
+updateTimes  = [stations(:).last_update];
+%  Diference between POSIX time and local time
+dif          = now - (java.lang.System.currentTimeMillis ...
+                     / (1000*60*60*24) + datenum('1970','yyyy'));
+%  Add the difference and convert to datenums
+localVect    = updateTimes ./ (1000*60*60*24) + datenum('1970','yyyy') + dif;
+%  Take the average time
+lastUpdate   = mean(localVect);
+
+%% Plot
+%  Options
+locations    = [stations(:).position];
+lat          = [locations(:).lat];
+long         = [locations(:).lng];
+sizes        = 1.8.*([stations(:).available_bikes]+13);
+colors       = [stations(:).available_bikes] ./ [stations(:).bike_stands];
+voronoiColor = [22 105 122]./255;
+textColor    = [186 216 228]./255;
+longLims     = [min(long)-0.005 max(long)+0.005];
+latLims      = [min(lat)-0.0005 max(lat)+0.0005];
+
+%  Prepare figure
+figure('DefaultTextFontName', 'Myriad Pro', ...
+    'DefaultTextFontSize', 5.5,...
+    'DefaultTextColor', textColor,...
+    'DefaultTextHorizontalAlignment', 'right',...
+    'DefaultTextUnits','normalized')
+hold on
+
+% Plot Voronoi diagram
+[x,y] = voronoi(long,lat);
+plot(x,y,'-','Color',voronoiColor)
+
+%  Plot stations
+scatter(long,lat,sizes,colors,'fill')
+
+%  Style figure
+set(gcf, 'Position', [0, 0, 700, 500]);
+set(gcf, 'Color', 'w');
+xlim(longLims)
+ylim(latLims)
+axis off
+
+%  Add stats
+text(0.01,0.01,{
+    ['Full stations: ' num2str(fullSt) ' (' num2str(fullStP) '%)']
+    ['Empty stations: ' num2str(emptySt) ' (' num2str(emptyStP) '%)']
+    ['Available bikes: ' num2str(availBikes) ' (' num2str(availBikesP) '%)']
+    ['Available stands: ' num2str(availStands) ' (' num2str(availStandsP) '%)']
+    % ['Most populated: ' mostPopSt]
+    'CC-BY 2.0 @JorgeGT - Data from developer.jcdecaux.com'
+    },'HorizontalAlignment','left','VerticalAlignment','bottom') %#ok<*UNRCH>
+
+%  Add title
+text(0.99,1,[contract ' Bike Network'],'FontSize',12,'VerticalAlignment','top')
+
+%  Add subtitles
+text(0.99,0.91,{
+    'Size: available bikes / Color: ocupation'
+    datestr(lastUpdate,'dd/mm/yy HH:MM')
+    },'HorizontalAlignment','right','FontSize',6)
+
+%  Add Google map
+%  http://www.mathworks.es/matlabcentral/fileexchange/27627-plot-google-map
+%  You can customize the map look adding a style parameter to the URL
+%  https://developers.google.com/maps/documentation/staticmaps/?csw=1#StyledMaps
+plot_google_map('showlabels',0)
+
+%  Export figure
+%  https://github.com/ojwoodford/export_fig
+export_fig(['JCDstatus_' contract],'-png','-zbuffer','-a3','-m2')
+
+%  Close all
+close all
---
+++ README.md
@@ -0,0 +1,66 @@
+JCDstatusPlot
+==========
+
+An example of how to access [JCDecaux Open Data API](https://developer.jcdecaux.com/#/opendata/vls?page=getstarted) from MATLAB. They offer real time status updates of their public bike services around the world, a very valuable information if one wishes to gain insight of city dynamics, transportation necessities, etc.
+
+### Requirements  
+Assuming that you have read the API docs and got a key with real-time data reading permissions, you need to save it in a matfile like this:
+
+```matlab
+apiKey = 'YOUR_KEY_HERE';
+save('apiKey.mat','apiKey');
+```
+
+The script can work as standalone, but I've used two libraries from the MathWorks Matlab Central which you can either download (their URLs are included in the source) or disable.
+
+> Tip: Browse Oliver O'Brien's [Bike Share Map](http://bikes.oobrien.com/global.php) for an example of a net web application built upon this data!
+
+### Example result
+
+This example script gets the status of the municipal bike service of the city of Valencia and plots a simple visualization of it, conveying the number of bikes available on each station, their ocupation rate, and some simple statistics. Also, a Voronoi diagram is plotted on top of it, showing the "cells" that each stations should, in theory, cover.
+
+<p align="center">
+    <img src="http://i.imgur.com/YFsOXMB.png" alt="Results"
+    width="70%" height="70%" />
+</p>
+
+
+
+### Further examples
+
+#### Global status
+
+As all covered cities are also accesible through the API, it's easy to loop through all of them in order to build a global visualization of the service status. You could use a code like this:
+
+```matlab
+url       = 'https://api.jcdecaux.com/vls/v1/contracts';
+rawData   = urlread([url '?apiKey=' apiKey]);
+contracts = cell2mat(loadjson(rawData));
+
+for i = 1:length(contracts)
+    try
+    % Make the script a function and comment out
+    % the contract name, passing it as an argument 
+    JCDstatusPlot_function(contracts(i).name)
+    catch error
+        disp(error)
+    end
+end
+```
+
+This should allow you to build a global snapshop like this one:
+
+<p align="center">
+    <img src="http://i.imgur.com/9ENp0jll.jpg" alt="Results"
+    width="70%" height="70%" />
+</p>
+
+#### Animation
+Another interesting option is to save a snapshot of the status of a contract every few minutes and then loop through the images in order to create an animation displaying the "flow" of people across the city. In this example, you can see people flocking to the city center and the northwest university campuses at around 8h-9h, going to the beach at around 16h, etc.
+
+<p align="center">
+<iframe src="http://gfycat.com/iframe/KeyEarnestAlleycat" frameborder="0" scrolling="no" width="478" height="342" ></iframe>
+</p>
+
+### Licensing
+This script is licensed under GPL v3.
